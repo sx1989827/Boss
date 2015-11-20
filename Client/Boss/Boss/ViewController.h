dba1af64c1ebf6ba3775ebf97b0188e237d32f1e
sx1989827 <395414574@qq.com>
2015-11-20 14:19:13 +0800

    add

---
+++ Client/Boss/Boss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Boss
+//
+//  Created by 孙昕 on 15/11/20.
+//  Copyright © 2015年 孙昕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Client/Boss/Boss/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Boss
+//
+//  Created by 孙昕 on 15/11/20.
+//  Copyright © 2015年 孙昕. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
