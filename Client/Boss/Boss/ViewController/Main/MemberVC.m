5a05b2883c713380bdf4af959335cdee2a8c8c21
sx1989827 <395414574@qq.com>
2015-12-07 18:03:04 +0800

    add]

--- Client/Boss/Boss/ViewController/Main/MemberVC.m
+++ Client/Boss/Boss/ViewController/Main/MemberVC.m
@@ -21,7 +21,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.bHud=NO;
-    self.title=@"我的";
     [self hideBackButton];
     [_tableMain setDelegateAndDataSource:self];
     [_tableMain registarCell:NSStringFromClass([UITableViewCell class]) StrItem:nil];
