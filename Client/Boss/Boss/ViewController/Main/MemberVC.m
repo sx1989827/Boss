a29101800d061d34de37e4b09c7ed53cca4c18f5
asd197203 <lxx@librucedeMacBook-Pro.local>
2015-12-08 17:37:38 +0800

    fix

--- Client/Boss/Boss/ViewController/Main/MemberVC.m
+++ Client/Boss/Boss/ViewController/Main/MemberVC.m
@@ -14,8 +14,12 @@
 #import "UserDefaults.h"
 #import "TipView.h"
 #import "EditPswReq.h"
-@interface MemberVC ()<UINavigationControllerDelegate,UIImagePickerControllerDelegate>
-
+#import "AnimateInOut.h"
+@interface MemberVC ()<UINavigationControllerDelegate,UIImagePickerControllerDelegate,UIViewControllerTransitioningDelegate>
+{
+    AnimateInOut *ani;
+    
+}
 @end
 
 @implementation MemberVC
@@ -74,17 +78,38 @@
             UserDefaults *user = [UserDefaults sharedInstance];
             user.resModel = nil;
             [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"userModel"];
-            Class cls=NSClassFromString(@"LoginVC");
-            UIViewController *vc=[[cls alloc] init];
-            UINavigationController *nav=[[UINavigationController alloc] initWithRootViewController:vc];
-            UIWindow *window = [UIApplication sharedApplication].keyWindow;
-            window.rootViewController=nav;
+            [weakSelf performSelector:@selector(exit) withObject:nil afterDelay:5];
+            
+            
+//            Class cls=NSClassFromString(@"LoginVC");
+//            UIViewController *vc=[[cls alloc] init];
+//            UINavigationController *nav=[[UINavigationController alloc] initWithRootViewController:vc];
+//            UIWindow *window = [UIApplication sharedApplication].keyWindow;
+//            window.rootViewController=nav;
+         
+           
         } NoBlock:^{
         }];
     }];
     [_tableMain reloadStatic];
-}
+    ani =[[AnimateInOut alloc]init];
 
+    
+}
+-(void)exit
+{
+    UIImage *img=[[UIApplication sharedApplication].keyWindow image];
+    UIImageView *view=[[UIImageView alloc] initWithImage:img];
+    view.frame=[UIScreen mainScreen].bounds;
+    view.layer.zPosition=FLT_MAX;
+    [[UIApplication sharedApplication].keyWindow addSubview:view];
+    [UIView transitionWithView:[UIApplication sharedApplication].keyWindow duration:1 options:UIViewAnimationOptionTransitionCrossDissolve animations:^{
+        view.alpha=0;
+    } completion:^(BOOL finished) {
+        [view removeFromSuperview];
+    }];
+     [self presentViewController:@"LoginVC" Param:nil];
+}
 -(void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
