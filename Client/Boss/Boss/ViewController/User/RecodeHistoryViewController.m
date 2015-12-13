7bb516b526cdd50110ef89f99239de7c245030bc
asd197203 <lxx@librucedeMacBook-Pro.local>
2015-12-13 13:37:29 +0800

    fix

--- Client/Boss/Boss/ViewController/User/RecodeHistoryViewController.m
+++ Client/Boss/Boss/ViewController/User/RecodeHistoryViewController.m
@@ -13,51 +13,91 @@
 #import "TypeView.h"
 #import "TypeReq.h"
 #import "SGSheetMenu.h"
+#import "DeleteHistoryReq.h"
 @interface RecodeHistoryViewController ()<LazyTableViewDelegate>
 {
     
     TypeView *_typeView;
     NSInteger _selected;
+    NSMutableArray *typeArray;
 }
 @end
 
 @implementation RecodeHistoryViewController
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.bHud =NO;
     _typeView = [[[NSBundle mainBundle] loadNibNamed:@"TypeView" owner:nil options:nil] lastObject];
+    UIButton*deleteButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    deleteButton.frame = CGRectMake(0, 0, 40, 25);
+    [deleteButton addTarget:self action:@selector(delegateHistory) forControlEvents:UIControlEventTouchUpInside];
+    [deleteButton setTitle:@"清空" forState:UIControlStateNormal];
+    UIBarButtonItem*rightItem = [[UIBarButtonItem alloc]initWithCustomView:deleteButton];
+    self.navigationItem.rightBarButtonItem = rightItem;
     [_typeView.typeSelectButton addTarget:self action:@selector(typeSelected:) forControlEvents:UIControlEventTouchUpInside];
-    self.navigationItem.titleView = _typeView;
-    [_mainTableView setDelegateAndDataSource:self];
-    [_mainTableView registarCell:@"HistoryCell" StrItem:@"HistoryItem"];
-    [_mainTableView setPageParam:@"page" Page:0];
-    [_mainTableView reloadRequest:[NSString stringWithFormat:@"%@/user/history",serverUrl] Param:@{@"username":USERNAME,@"pwd":PWD,@"type":_type}];
+     [self loadRequest];
+   
+   
+    
 }
 -(NSArray*)LazyTableViewDidFinishRequest:(LazyTableView *)tableview Request:(NSDictionary *)dic
 {
     return dic[@"data"];
 }
+-(void)loadRequest
+{
+    typeArray =[[NSMutableArray alloc]initWithCapacity:0];
+    [TypeReq do:^(id req) {
+        TypeReq*obj =req;
+        obj.username =USERNAME;
+        obj.pwd =PWD;
+    } Res:^(id res) {
+        [self removeHud];
+        TypeRes *obj =res;
+        NSArray *typeModelArray = obj.data;
+        [typeModelArray enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            TypeModel*model =obj;
+            [typeArray addObject:model.name];
+        }];
+        _type = typeArray[0];
+        [_typeView.typeSelectButton setTitle:_type forState:UIControlStateNormal];
+        self.navigationItem.titleView = _typeView;
+        [_mainTableView setDelegateAndDataSource:self];
+        [_mainTableView registarCell:@"HistoryCell" StrItem:@"HistoryItem"];
+        [_mainTableView setPageParam:@"page" Page:0];
+        [_mainTableView reloadRequest:[NSString stringWithFormat:@"%@/user/history",serverUrl] Param:@{@"username":USERNAME,@"pwd":PWD,@"type":_type}];
+    } ShowHud:NO];
+}
+-(void)delegateHistory
+{
+    [DeleteHistoryReq do:^(id req) {
+        DeleteHistoryReq*obj =req;
+        obj.username = USERNAME;
+        obj.type = _typeView.typeSelectButton.titleLabel.text;
+    } Res:^(id res) {
+        DeleteHistoryRes*obj =res;
+        if(obj.code==0)
+        {
+            S(obj.msg);
+           
+            [_mainTableView reload];
+        }
+        else
+        {
+            E(obj.msg);
+        }
+    } ShowHud:NO];
+}
 -(void)typeSelected:(UIButton*)sender
 {
-             NSMutableArray *typeArray =[[NSMutableArray alloc]initWithCapacity:0];
-            [TypeReq do:^(id req) {
-                TypeReq*obj =req;
-                obj.username =USERNAME;
-                obj.pwd =PWD;
-            } Res:^(id res) {
-                TypeRes *obj =res;
-                NSArray *typeModelArray = obj.data;
-                [typeModelArray enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-                    TypeModel*model =obj;
-                    [typeArray addObject:model.name];
-                }];
                 [SGActionView showSheetWithTitle:@"选择类型" itemTitles:(NSArray*)typeArray selectedIndex:_selected selectedHandle:^(NSInteger index) {
                     _selected =index;
                     [_typeView.typeSelectButton setTitle:[NSString stringWithFormat:@"%@",typeArray[index]] forState:UIControlStateNormal];
                     _type =typeArray[index];
+                    [_mainTableView getParam][@"type"]=_type;
                     [_mainTableView reload];
                 }];
-            } ShowHud:YES];
+           
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
